d839793456a506fcd69a865c00c453caead91a74
Alex P <apputtre@gmail.com>
2026-04-20 13:38:23 -0400

    Rewrite vector scalar addition test

--- test/src/main.cpp
+++ test/src/main.cpp
@@ -656,6 +656,28 @@ void SuiteVecScalarAddition(TestEnvironment& tenv)
 
         tenv.assertEq(v, v_old + x);
     }
+
+    {
+        tenv.beginTest("Vector-scalar addition - different types 1");
+
+        vec<3, float> v {0.1, -0.2, 0.3};
+        double x = 2.5;
+
+        auto result = v + x;
+
+        tenv.assert(std::is_same<decltype(result), vec<3, double>>::value);
+
+        tenv.assertEq(result, vec<3, double>(v[0] + x, v[1] + x, v[2] + x));
+
+        // test commutativity
+        tenv.assertEq(v + x, x + v);
+
+        auto v_old = v;
+
+        v += x;
+
+        tenv.assertEq(v, (vec<3, double>) (v_old + x));
+    }
 }
 
 int main()
