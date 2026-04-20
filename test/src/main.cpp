423f00c0a97472bfc6a205c4b779dcb88f7590f4
Alex P <apputtre@gmail.com>
2026-04-20 14:16:46 -0400

    Added vector-vector subtraction test

--- test/src/main.cpp
+++ test/src/main.cpp
@@ -754,6 +754,23 @@ void SuiteVecSubtraction(TestEnvironment& tenv)
 
         tenv.assertEq(v1, v1_old - v2);
     }
+
+    {
+        tenv.beginTest("Vector-vector subtraction - same types 2");
+
+        vec<2, float> v1 {2.5, -3.7};
+        vec<2, float> v2 {13.6, 4.5};
+
+        tenv.assert(std::is_same<decltype(v1 - v2), vec<2, float>>::value);
+        tenv.assertEq(v1 - v2, vec<2, float>(v1[0] - v2[0], v1[1] - v2[1]));
+        // test anti-commutativity
+        tenv.assertEq(v1 - v2, -(v2 - v1));
+
+        vec<2, float> v1_old = v1;
+        v1 -= v2;
+
+        tenv.assertEq(v1, v1_old - v2);
+    }
 }
 
 int main()
