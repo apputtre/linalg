5a541d5537a02d42bd1b5bd053dd4ba5f12ee972
Alex P <apputtre@gmail.com>
2026-04-20 17:40:39 -0400

    Added dot product test

--- test/src/main.cpp
+++ test/src/main.cpp
@@ -961,13 +961,15 @@ void SuiteVecDotProduct(TestEnvironment& tenv)
     tenv.beginSuite("Vector dot product");
 
     {
-        tenv.beginTest("Dot product - Test 1");
+        tenv.beginTest("Dot product - same types 1");
 
         vec<2, int> v1(1, 2);
         vec<2, int> v2(3, 4);
 
         tenv.assertEq(v1 * v2, v1[0] * v2[0] + v1[1] * v2[1]);
         tenv.assertEq(v1 * v2, v2 * v1);
+
+        tenv.assertNeq(v1 * v2, 0);
     }
 }
 
