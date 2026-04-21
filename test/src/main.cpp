e1f6d920484d2a418b6a8349c99c1dbad1ade248
Alex P <apputtre@gmail.com>
2026-04-21 03:19:22 -0400

    Implemented floatCompare

--- test/src/main.cpp
+++ test/src/main.cpp
@@ -5,7 +5,42 @@
 
 #include "TestEnvironment.h"
 
+#include <float.h>
+#include <bit>
+
 using namespace linalg;
+
+bool floatCompare(double x, double y)
+{
+    for (size_t i = 0; i < sizeof(std::declval<double>()); ++i)
+    {
+        unsigned int bx, by;
+
+        memcpy(&bx, (uint8_t*) &x + i, 1);
+        memcpy(&by, (uint8_t*) &y + i, 1);
+
+        if (std::endian::native == std::endian::little)
+        {
+            if (i == 0)
+            {
+                // compare all but the first bit
+                if (bx >> 1 != by >> 1)
+                    return false;
+            }
+            else
+            {
+                if (bx != by)
+                    return false;
+            }
+        }
+        else
+        {
+            throw std::runtime_error("Not implemented");
+        }
+    }
+
+    return true;
+}
 
 void SuiteVecElemAccessConstruction(TestEnvironment& tenv)
 {
