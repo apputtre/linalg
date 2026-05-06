a1864b2df79e0d26c502b4d46ab3f03ba0dd6f4d
Alex P <apputtre@gmail.com>
2026-05-06 13:54:07 -0400

    Added submat vector assignment test

--- test/include/SubmatTests.h
+++ test/include/SubmatTests.h
@@ -49,4 +49,16 @@ SUITE(submatAssignment,
             assertEq(elems[1], 137);
             assertEq(elems[3], 137);
         })
+
+        TEST(submatVectorAssignment,
+        {
+            int elems[] = {11, 12, 21, 22};
+
+            submat s(&elems[1], 2, 2);
+
+            s = vec<2, int> {137, 314};
+
+            assertEq(elems[1], 137);
+            assertEq(elems[3], 314);
+        })
     })
