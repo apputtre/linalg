f0d0b235d01efdf6d5c1d07b5659dc9ac603b4b1
Alex P <apputtre@gmail.com>
2026-05-07 11:52:22 -0400

    Revert "Removed unnecessary constraint on submat::op=(vec)"
    This reverts commit 58a85365db0ada3c54d40199215d6a516d94a08f.

--- include/submat.h
+++ include/submat.h
@@ -41,6 +41,7 @@ struct submat
     }
 
     template<typename TOther>
+        requires (!std::same_as<T, vec<L, TOther>>)
     submat& operator=(const TOther& scalar)
     {
         for (size_t i = 0; i < L; ++i)
