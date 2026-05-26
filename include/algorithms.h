7e0cc9875d2605b7c5a6397e887d891e9f2a566e
Alex P <apputtre@gmail.com>
2026-05-26 15:23:05 -0400

    Implemented inv for 2x2 matrix

--- include/algorithms.h
+++ include/algorithms.h
@@ -8,6 +8,22 @@ namespace linalg
     template<size_t Dim, typename T>
     mat<Dim, Dim, T> inv(const mat<Dim, Dim, T>& m)
     {
+        if (Dim == 2)
+        {
+            T det = m[0][0] * m[1][1] - m[0][1] * m[1][0];
+
+            if (det == 0)
+            {
+                // matrix has no inverse
+                return mat<Dim, Dim, T>();
+            }
+
+            return {
+                {1 / det * m[1][1], 1 / det * -m[0][1]},
+                {1 / det * -m[1][0], 1 / det * m[0][0]}
+            };
+        }
+
         return m;
     }
 }
