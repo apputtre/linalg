81fe66b99955c51310c484a1f9d07bd9be5827ec
Alex P <apputtre@gmail.com>
2026-05-29 17:07:50 -0400

    Added a test which casuses current LU implementation to fail

--- test/include/development_tests/LU_factorization.h
+++ test/include/development_tests/LU_factorization.h
@@ -28,4 +28,29 @@ SUITE("LU Factorization")
             {0, 0, 3}
         });
     }
+
+    TEST("Test 2")
+    {
+        linalg::mat<3, 3, float> m = {
+            {1, 0, 0},
+            {3, 0, -1},
+            {2, 1, -1}
+        };
+
+        linalg::mat<3, 3, float> L;
+        linalg::mat<3, 3, float> U;
+        linalg::luf(m, L, U);
+
+        assertEq(L, linalg::mat<3, 3, float> {
+            {1, 0, 0},
+            {3, 1, 0},
+            {2, 0, 1}
+        });
+
+        assertEq(U, linalg::mat<3, 3, float> {
+            {1, 0, 0},
+            {0, 1, -1},
+            {0, 0, -1}
+        });
+    }
 }
